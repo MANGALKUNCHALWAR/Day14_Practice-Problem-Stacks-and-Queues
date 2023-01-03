0b806539904b017e6889846bcd9a06671f5d2904
John Doe <mangalkunchalwar9999@gmail.com>
2023-01-03 21:46:09 +0530

    welcome to Practice Problem of Stacks and Queues

---
+++ Main.java
@@ -0,0 +1,7 @@
+package assignmentday14.practiceproblemStacksandQueues;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("welcome to Practice Problem of Stacks and Queues");
+    }
+}
